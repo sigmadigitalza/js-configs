6d80c869ee6b98a47152df92c1f2bd4efcc069c6
pushplaybang <pushplaybang@gmail.com>
2019-01-29 17:17:40 +0200

    init && scaffold

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  'babel-preset': require('presets/babel'),
+  'eslint-preset': require('presets/eslint'),
+  'prettier-preset': require('presets/prettier'),
+  'stylelint-preset': require('presets/stylelint'),
+};
