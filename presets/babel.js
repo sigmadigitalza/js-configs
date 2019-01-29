601144d2d58362162a2613f9ca2bf7a9693c28eb
pushplaybang <pushplaybang@gmail.com>
2019-01-29 21:13:54 +0200

    babel preset v1

---
+++ presets/babel.js
@@ -0,0 +1,43 @@
+module.exports = {
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "targets": {
+          "browsers": [
+            "> 1%",
+            "last 4 versions",
+            "safari >= 7"
+          ],
+          "node": "8.11.3"
+        },
+        "useBuiltIns": "usage"
+      }
+    ],
+    [
+      "@babel/preset-react"
+    ],
+    [
+      "@babel/preset-flow"
+    ],
+    [
+      "minify"
+    ]
+  ],
+  "plugins": [
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-syntax-import-meta",
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-json-strings",
+    [
+      "@babel/plugin-proposal-decorators",
+      {
+        "legacy": true
+      }
+    ],
+    "@babel/plugin-proposal-function-sent",
+    "@babel/plugin-proposal-export-namespace-from",
+    "@babel/plugin-proposal-numeric-separator",
+    "@babel/plugin-proposal-throw-expressions"
+  ]
+};
