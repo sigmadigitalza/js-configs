c204d5ce0795aef5234f2e677286bce52dcacde7
pushplaybang <pushplaybang@gmail.com>
2019-01-30 02:16:54 +0200

    missing closing parenthesis

--- install.js
+++ install.js
@@ -5,12 +5,12 @@ if (process.env.NODE_ENV !== 'production') {
   const path = require('path');
   const sh = require('shelljs');
 
-  sh.cp( path.join(__dirname, './configFiles', '.babelrc'), path.join(__dirname, '../../../');
-  sh.cp( path.join(__dirname, './configFiles', '.editorconfig'), path.join(__dirname, '../../../');
-  sh.cp( path.join(__dirname, './configFiles', '.eslintignore'), path.join(__dirname, '../../../');
-  sh.cp( path.join(__dirname, './configFiles', '.flowconfig'), path.join(__dirname, '../../../');
-  sh.cp( path.join(__dirname, './configFiles', '.prettierrc.js'), path.join(__dirname, '../../../');
-  sh.cp( path.join(__dirname, './configFiles', '.stylelintrc'), path.join(__dirname, '../../../');
+  sh.cp( path.join(__dirname, './configFiles', '.babelrc'), path.join(__dirname, '../../../'));
+  sh.cp( path.join(__dirname, './configFiles', '.editorconfig'), path.join(__dirname, '../../../'));
+  sh.cp( path.join(__dirname, './configFiles', '.eslintignore'), path.join(__dirname, '../../../'));
+  sh.cp( path.join(__dirname, './configFiles', '.flowconfig'), path.join(__dirname, '../../../'));
+  sh.cp( path.join(__dirname, './configFiles', '.prettierrc.js'), path.join(__dirname, '../../../'));
+  sh.cp( path.join(__dirname, './configFiles', '.stylelintrc'), path.join(__dirname, '../../../'));
 
   console.log('@sigmadigital/configs installed successfully');
 }
