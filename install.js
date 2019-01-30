7b59c828d964b81b74191d427b2a3b9fff1927a3
pushplaybang <pushplaybang@gmail.com>
2019-01-30 16:31:03 +0200

    install script now skips existing files

--- install.js
+++ install.js
@@ -1,17 +1,39 @@
+const path = require('path');
+const sh = require('shelljs');
+
+// check if there is a backup directory, and if so
+// move the existing file there.
+const addConfigFileToRoot = fileName => {
+
+  // check if file exists
+  if (sh.test('-f', path.join(__dirname, '../../../', fileName))) {
+    console.log(`skiped adding ${fileName} to root as ${fileName} already exists`);
+    return;
+  }
+
+  // otherwise create new
+  sh.cp( path.join(__dirname, './configFiles', fileName), path.join(__dirname, '../../../'));
+};
+
+// for each string in the array, call addConfigFileToRoot
+const addListOfConfigFilesToRoot = filesArr => filesArr.map(f => addConfigFileToRoot(f));
+
+
 // @TODO: these should not be tasks that autorun, this should be the work of a basic CLI
 if (process.env.NODE_ENV !== 'production') {
   console.log('copying over config files, this will replace existing configs.');
 
-  const path = require('path');
-  const sh = require('shelljs');
+  const configs = [
+    '.babelrc',
+    '.eslintrc.json',
+    '.editorconfig',
+    '.eslintignore',
+    '.flowconfig',
+    '.prettierrc.js',
+    '.stylelintrc',
+  ];
 
-  sh.cp( path.join(__dirname, './configFiles', '.babelrc'), path.join(__dirname, '../../../'));
-  sh.cp( path.join(__dirname, './configFiles', '.eslintrc.json'), path.join(__dirname, '../../../'));
-  sh.cp( path.join(__dirname, './configFiles', '.editorconfig'), path.join(__dirname, '../../../'));
-  sh.cp( path.join(__dirname, './configFiles', '.eslintignore'), path.join(__dirname, '../../../'));
-  sh.cp( path.join(__dirname, './configFiles', '.flowconfig'), path.join(__dirname, '../../../'));
-  sh.cp( path.join(__dirname, './configFiles', '.prettierrc.js'), path.join(__dirname, '../../../'));
-  sh.cp( path.join(__dirname, './configFiles', '.stylelintrc'), path.join(__dirname, '../../../'));
+  addListOfConfigFilesToRoot(configs);
 
   console.log('@sigmadigital/configs installed successfully');
 }
