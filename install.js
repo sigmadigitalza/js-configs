68f34729044958b2bd523f0dfaf5a654c66cf283
pushplaybang <pushplaybang@gmail.com>
2019-01-30 02:09:42 +0200

    install to project root :/

--- install.js
+++ install.js
@@ -1,15 +1,16 @@
+// @TODO: these should not be tasks that autorun, this should be the work of a basic CLI
 if (process.env.NODE_ENV !== 'production') {
   console.log('copying over config files, this will replace existing configs.');
 
   const path = require('path');
   const sh = require('shelljs');
 
-  sh.cp( path.join(__dirname, './configFiles', '.babelrc'), path.dirname(require.main.filename));
-  sh.cp( path.join(__dirname, './configFiles', '.editorconfig'), path.dirname(require.main.filename));
-  sh.cp( path.join(__dirname, './configFiles', '.eslintignore'), path.dirname(require.main.filename));
-  sh.cp( path.join(__dirname, './configFiles', '.flowconfig'), path.dirname(require.main.filename));
-  sh.cp( path.join(__dirname, './configFiles', '.prettierrc.js'), path.dirname(require.main.filename));
-  sh.cp( path.join(__dirname, './configFiles', '.stylelintrc'), path.dirname(require.main.filename));
+  sh.cp( path.join(__dirname, './configFiles', '.babelrc'), path.join(__dirname, '../../../');
+  sh.cp( path.join(__dirname, './configFiles', '.editorconfig'), path.join(__dirname, '../../../');
+  sh.cp( path.join(__dirname, './configFiles', '.eslintignore'), path.join(__dirname, '../../../');
+  sh.cp( path.join(__dirname, './configFiles', '.flowconfig'), path.join(__dirname, '../../../');
+  sh.cp( path.join(__dirname, './configFiles', '.prettierrc.js'), path.join(__dirname, '../../../');
+  sh.cp( path.join(__dirname, './configFiles', '.stylelintrc'), path.join(__dirname, '../../../');
 
   console.log('@sigmadigital/configs installed successfully');
 }
