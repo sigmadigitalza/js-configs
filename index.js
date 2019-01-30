70d2c1d0fc9299e0ac0dd28fa4d7a7467ce9eb8e
pushplaybang <pushplaybang@gmail.com>
2019-01-30 03:25:14 +0200

    extracted and renamed eslint config

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
   'babel': require('presets/babel'),
-  'eslint': require('presets/eslint'),
   'prettier': require('presets/prettier'),
   'stylelint': require('presets/stylelint'),
 };
